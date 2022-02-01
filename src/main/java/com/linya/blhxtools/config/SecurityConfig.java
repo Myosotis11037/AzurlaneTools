4360b2ac3f0c4b69f8e05c7a2bb9983f466dcf03
林丝瑾 <kiljaeden0905@gmail.com>
2022-02-01 21:09:16 +0800

    将数据库密码加密校验的方式从明文比照改为了BCrypt

--- src/main/java/com/linya/blhxtools/config/SecurityConfig.java
+++ src/main/java/com/linya/blhxtools/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 
@@ -24,13 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/user").permitAll()
-//                .antMatchers("/**").authenticated()
-                .and()
+        http.formLogin()
+                .loginProcessingUrl("/login")
+            .and()
+                .authorizeRequests()
+                .antMatchers(URL_WHITELIST).permitAll()
+                .antMatchers("/update/*").hasAuthority("admin")
+                .anyRequest().authenticated()
+            .and()
                 .httpBasic().disable()
-                .formLogin()
-                .and()
                 .addFilterAfter(myFilter, CsrfFilter.class);
     }
 
@@ -41,17 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword + "";
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return encodedPassword.equals(encode(rawPassword));
-            }
-        };
+        return new BCryptPasswordEncoder();
     }
+
+    private static final String[] URL_WHITELIST = {
+            "test",
+            "login"
+    };
 
 }
