edac32e7e973eccdb7c791bbb860019f7f737b17
林丝瑾 <kiljaeden0905@gmail.com>
2022-02-01 19:54:46 +0800

    修改了舰炮数据库接口

--- src/main/java/com/linya/blhxtools/dao/CarronadeDao.java
+++ src/main/java/com/linya/blhxtools/dao/CarronadeDao.java
@@ -3,10 +3,6 @@ package com.linya.blhxtools.dao;
 import com.linya.blhxtools.entity.Carronade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-
-// spring jpa
 
 public interface CarronadeDao extends JpaRepository<Carronade, String> {
 //
