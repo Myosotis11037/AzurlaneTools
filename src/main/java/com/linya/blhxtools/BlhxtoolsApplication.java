318046145d8a4f79e87d6991a28c700f8e5cf804
林丝瑾 <76152401+Myosotis11037@users.noreply.github.com>
2022-02-20 15:45:16 +0800

    test

--- src/main/java/com/linya/blhxtools/BlhxtoolsApplication.java
+++ src/main/java/com/linya/blhxtools/BlhxtoolsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlhxtoolsApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlhxtoolsApplication.class, args);
 	}
